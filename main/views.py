2b2f293e59854984f489a7231d53525394de2b9d
Amertaaa <syahrulkhaliq58@gmail.com>
2023-09-27 13:50:14 +0700

    tutor4

--- main/views.py
+++ main/views.py
@@ -37,17 +37,38 @@ def show_main(request):
     return render(request, "main.html", context)
     
 def create_product(request):
- form = ProductForm(request.POST or None)
+    form = ProductForm(request.POST or None)
 
- if form.is_valid() and request.method == "POST":
-     product = form.save(commit=False)
-     product.user = request.user
-     product.save()
-     return HttpResponseRedirect(reverse('main:show_main'))
+    if form.is_valid() and request.method == "POST":
+        product = form.save(commit=False)
+        product.user = request.user
+        product.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+    context = {'form': form}
+    return render(request, "create_product.html", context)
 # Create your views here.
+def edit_product(request, id):
+    # Get product berdasarkan ID
+    product = Product.objects.get(pk = id)
 
+    # Set product sebagai instance dari form
+    form = ProductForm(request.POST or None, instance=product)
+
+    if form.is_valid() and request.method == "POST":
+        # Simpan form dan kembali ke halaman awal
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "edit_product.html", context)
     
-
+def delete_product(request, id):
+    # Get data berdasarkan ID
+    product = Product.objects.get(pk = id)
+    # Hapus data
+    product.delete()
+    # Kembali ke halaman awal
+    return HttpResponseRedirect(reverse('main:show_main'))
     
 
 def show_xml(request):
